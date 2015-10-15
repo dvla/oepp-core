8ffb1234a58c993f7fdd52469b6a2b231ace2c5c
Damian Dunajski <d.dunajski@kainos.com>
2015-10-15 11:42:56 +0100

    Moved ServerCommand and related bits and pieces to AbstractClient

--- src/main/java/uk/gov/dvla/core/client/AbstractServiceClient.java
+++ src/main/java/uk/gov/dvla/core/client/AbstractServiceClient.java
@@ -1,9 +1,19 @@
 package uk.gov.dvla.core.client;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import uk.gov.dvla.core.exception.RequestProcessingException;
+import uk.gov.dvla.core.exception.UnexpectedResponseException;
+
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.client.Client;
+import javax.ws.rs.core.Response;
 import java.net.URL;
+import java.util.Optional;
 
 public abstract class AbstractServiceClient {
+
+    private static final Logger logger = LoggerFactory.getLogger(AbstractServiceClient.class);
 
     protected static final String HEALTH_CHECK_URI = "/healthcheck";
 
@@ -19,4 +29,32 @@ public abstract class AbstractServiceClient {
         return (baseURL + "/" + path).replaceAll("([^:]\\/)\\/+", "$1");
     }
 
+    protected <Req, Res> Optional<Res> callServerForOptionalResource(ServerCommand<Req, Res> serverCommand, Req request) {
+        try {
+            return Optional.of(callServerForMandatoryResource(serverCommand, request));
+        } catch (UnexpectedResponseException ex) {
+            if (ex.getResponseStatus() == 404) {
+                return Optional.empty();
+            }
+            throw ex;
+        }
+    }
+
+    protected <Req, Res> Res callServerForMandatoryResource(ServerCommand<Req, Res> serverCommand, Req request) {
+        try {
+            return serverCommand.makeServerCall(request);
+        } catch (WebApplicationException ex) {
+            Response errorResponse = ex.getResponse();
+            logger.error("Underlying service returned " + errorResponse.getStatus() + " response for request: " + request, ex);
+            throw new UnexpectedResponseException(errorResponse.getStatus(), errorResponse.readEntity(String.class), ex);
+        } catch (Exception ex) {
+            logger.error("Unexpected error occurred while calling underlying service for request: " + request, ex);
+            throw new RequestProcessingException(ex);
+        }
+    }
+
+    protected interface ServerCommand<Req, Res> {
+
+        Res makeServerCall(Req req);
+    }
 }
